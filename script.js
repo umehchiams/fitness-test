5a997cd1d42fefdf1f779fb84049cb989371c930
umehchiams <119131956+umehchiams@users.noreply.github.com>
2022-12-01 20:47:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const change = document. getElementById ("IMAGE1");
+
+change.addEventListener("click", (Event) => {
+    Event.target.style.background= "red";
+Event.target.style.colour = "white";
+});
+
+const change2= document. getElementById ("IMAGE2");
+
+change.addEventListener("click", (Event)=> {
+   Event.target.style.background ="blue";
+    Event.target.style.colour="green";
+});
+
+const change3= document. getElementById ("IMAGE3");
+Event.TARGET.TYLE.BACKGROUND="YELLOW";
+Event.target.style.colour="purple"
